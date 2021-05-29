8ab53ca2416fc2083a4f8b118aaeafdcb4d071f1
Cloudz-N <82699510+Cloudz-N@users.noreply.github.com>
2021-05-29 13:11:32 +0800

    Tictactoe

---
+++ TICTACTOE/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace TICTACTOE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Box1 = new System.Windows.Forms.Button();
+            this.Box2 = new System.Windows.Forms.Button();
+            this.Box3 = new System.Windows.Forms.Button();
+            this.Box4 = new System.Windows.Forms.Button();
+            this.Box5 = new System.Windows.Forms.Button();
+            this.Box6 = new System.Windows.Forms.Button();
+            this.Box7 = new System.Windows.Forms.Button();
+            this.Box8 = new System.Windows.Forms.Button();
+            this.Box9 = new System.Windows.Forms.Button();
+            this.Xscore = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Oscore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.theme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Box1
+            // 
+            this.Box1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box1.Location = new System.Drawing.Point(12, 12);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(80, 80);
+            this.Box1.TabIndex = 0;
+            this.Box1.UseVisualStyleBackColor = true;
+            this.Box1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box2
+            // 
+            this.Box2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box2.Location = new System.Drawing.Point(98, 12);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(80, 80);
+            this.Box2.TabIndex = 1;
+            this.Box2.UseVisualStyleBackColor = true;
+            this.Box2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box3
+            // 
+            this.Box3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box3.Location = new System.Drawing.Point(184, 12);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(80, 80);
+            this.Box3.TabIndex = 2;
+            this.Box3.UseVisualStyleBackColor = true;
+            this.Box3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box4
+            // 
+            this.Box4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box4.Location = new System.Drawing.Point(12, 98);
+            this.Box4.Name = "Box4";
+            this.Box4.Size = new System.Drawing.Size(80, 80);
+            this.Box4.TabIndex = 3;
+            this.Box4.UseVisualStyleBackColor = true;
+            this.Box4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box5
+            // 
+            this.Box5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box5.Location = new System.Drawing.Point(98, 98);
+            this.Box5.Name = "Box5";
+            this.Box5.Size = new System.Drawing.Size(80, 80);
+            this.Box5.TabIndex = 4;
+            this.Box5.UseVisualStyleBackColor = true;
+            this.Box5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box6
+            // 
+            this.Box6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box6.Location = new System.Drawing.Point(184, 98);
+            this.Box6.Name = "Box6";
+            this.Box6.Size = new System.Drawing.Size(80, 80);
+            this.Box6.TabIndex = 5;
+            this.Box6.UseVisualStyleBackColor = true;
+            this.Box6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box7
+            // 
+            this.Box7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box7.Location = new System.Drawing.Point(12, 184);
+            this.Box7.Name = "Box7";
+            this.Box7.Size = new System.Drawing.Size(80, 80);
+            this.Box7.TabIndex = 6;
+            this.Box7.UseVisualStyleBackColor = true;
+            this.Box7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box8
+            // 
+            this.Box8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box8.Location = new System.Drawing.Point(98, 184);
+            this.Box8.Name = "Box8";
+            this.Box8.Size = new System.Drawing.Size(80, 80);
+            this.Box8.TabIndex = 7;
+            this.Box8.UseVisualStyleBackColor = true;
+            this.Box8.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Box9
+            // 
+            this.Box9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Box9.Location = new System.Drawing.Point(184, 184);
+            this.Box9.Name = "Box9";
+            this.Box9.Size = new System.Drawing.Size(80, 80);
+            this.Box9.TabIndex = 8;
+            this.Box9.UseVisualStyleBackColor = true;
+            this.Box9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Xscore
+            // 
+            this.Xscore.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Xscore.Location = new System.Drawing.Point(283, 126);
+            this.Xscore.Name = "Xscore";
+            this.Xscore.Size = new System.Drawing.Size(100, 63);
+            this.Xscore.TabIndex = 10;
+            this.Xscore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Xscore.TextChanged += new System.EventHandler(this.Xscore_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(373, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Scores";
+            // 
+            // Oscore
+            // 
+            this.Oscore.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Oscore.Location = new System.Drawing.Point(437, 126);
+            this.Oscore.Name = "Oscore";
+            this.Oscore.Size = new System.Drawing.Size(100, 63);
+            this.Oscore.TabIndex = 12;
+            this.Oscore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Oscore.TextChanged += new System.EventHandler(this.Oscore_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(312, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 55);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(456, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 55);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "O";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(283, 207);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(100, 23);
+            this.Reset.TabIndex = 15;
+            this.Reset.Text = "New Game";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(437, 207);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(100, 23);
+            this.Clear.TabIndex = 16;
+            this.Clear.Text = "Clear Score";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // theme
+            // 
+            this.theme.Location = new System.Drawing.Point(357, 236);
+            this.theme.Name = "theme";
+            this.theme.Size = new System.Drawing.Size(108, 23);
+            this.theme.TabIndex = 17;
+            this.theme.Text = "Change Color";
+            this.theme.UseVisualStyleBackColor = true;
+            this.theme.Click += new System.EventHandler(this.theme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 277);
+            this.Controls.Add(this.theme);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Oscore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Xscore);
+            this.Controls.Add(this.Box9);
+            this.Controls.Add(this.Box8);
+            this.Controls.Add(this.Box7);
+            this.Controls.Add(this.Box6);
+            this.Controls.Add(this.Box5);
+            this.Controls.Add(this.Box4);
+            this.Controls.Add(this.Box3);
+            this.Controls.Add(this.Box2);
+            this.Controls.Add(this.Box1);
+            this.Name = "Form1";
+            this.Text = "TIC TAC TOE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Box1;
+        private System.Windows.Forms.Button Box2;
+        private System.Windows.Forms.Button Box3;
+        private System.Windows.Forms.Button Box4;
+        private System.Windows.Forms.Button Box5;
+        private System.Windows.Forms.Button Box6;
+        private System.Windows.Forms.Button Box7;
+        private System.Windows.Forms.Button Box8;
+        private System.Windows.Forms.Button Box9;
+        private System.Windows.Forms.TextBox Xscore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Oscore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button theme;
+    }
+}
+
---
+++ TICTACTOE/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TICTACTOE
+{
+    public partial class Form1 : Form
+    {
+        bool Xturn = true;
+        byte Xturn_count = 0;
+        int X = 0;
+        int O = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cscores();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button Turn = (Button)sender;
+            if (Xturn)
+                Turn.Text = "X";
+            else
+                Turn.Text = "O";
+
+            Xturn = !Xturn;
+            Turn.Enabled = false;
+
+            Win();
+        }
+
+        private void Win()
+        {
+            bool Winner = false;
+
+            if ((Box1.Text == Box2.Text) && (Box2.Text == Box3.Text) && (!Box1.Enabled)) // 1-3
+                Winner = true;
+            else if ((Box1.Text == Box5.Text) && (Box5.Text == Box9.Text) && (!Box1.Enabled)) // 1-9
+                Winner = true;
+            else if ((Box1.Text == Box4.Text) && (Box4.Text == Box7.Text) && (!Box1.Enabled)) // 1-7
+                Winner = true;
+            else if((Box2.Text == Box5.Text) && (Box5.Text == Box8.Text) && (!Box2.Enabled)) // 2-8
+                Winner = true;
+            else if((Box3.Text == Box6.Text) && (Box6.Text == Box9.Text) && (!Box3.Enabled)) // 3-9
+                Winner = true;
+            else if((Box3.Text == Box5.Text) && (Box5.Text == Box7.Text) && (!Box3.Enabled)) // 3-7
+                Winner = true;
+            else if((Box4.Text == Box5.Text) && (Box5.Text == Box6.Text) && (!Box4.Enabled)) // 4-6
+                Winner = true;
+            else if((Box7.Text == Box8.Text) && (Box8.Text == Box9.Text) && (!Box7.Enabled)) // 7-9
+                Winner = true;
+
+            if (Winner)
+            {
+                if (Xturn == true)
+                {
+                    MessageBox.Show("O WON!");
+                    O += 1;
+                    Oscore.Text = O.ToString();
+                    newgame();
+                }
+                else if (Xturn == false)
+                {
+                    MessageBox.Show("X WON!");
+                    X += 1;
+                    Xscore.Text = X.ToString();
+                    newgame();
+                }
+                else if (Xturn_count == 9)
+                {
+                    MessageBox.Show("XO DRAW!");
+                    newgame();
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            newgame();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Xscore.Clear();
+            Oscore.Clear();
+            Cscores();
+        }
+
+        private void newgame()
+        {
+            Xturn = true;
+            Xturn_count = 0;
+            Box1.Enabled = true;
+            Box1.Text = "";
+            Box2.Enabled = true;
+            Box2.Text = "";
+            Box3.Enabled = true;
+            Box3.Text = "";
+            Box4.Enabled = true;
+            Box4.Text = "";
+            Box5.Enabled = true;
+            Box5.Text = "";
+            Box6.Enabled = true;
+            Box6.Text = "";
+            Box7.Enabled = true;
+            Box7.Text = "";
+            Box8.Enabled = true;
+            Box8.Text = "";
+            Box9.Enabled = true;
+            Box9.Text = "";
+        }
+
+        private void Xscore_TextChanged(object sender, EventArgs e)
+        {
+            Xscore.ReadOnly = true;
+        }
+
+        private void Oscore_TextChanged(object sender, EventArgs e)
+        {
+            Oscore.ReadOnly = true;
+        }
+
+        private void theme_Click(object sender, EventArgs e)
+        {
+            Box1.Enabled = false;
+            Box2.Enabled = false;
+            Box3.Enabled = false;
+            Box4.Enabled = false;
+            Box5.Enabled = false;
+            Box6.Enabled = false;
+            Box7.Enabled = false;
+            Box8.Enabled = false;
+            Box9.Enabled = false;
+            Random colors = new Random();
+            Color clr = Color.FromArgb(colors.Next(0, 255));
+            Box1.BackColor = clr;
+            Box2.BackColor = clr;
+            Box3.BackColor = clr;
+            Box4.BackColor = clr;
+            Box5.BackColor = clr;
+            Box6.BackColor = clr;
+            Box7.BackColor = clr;
+            Box8.BackColor = clr;
+            Box9.BackColor = clr;
+            newgame();
+        }
+        private void Cscores()
+        {
+            Oscore.Text = O.ToString();
+            Xscore.Text = X.ToString();
+        }
+    }
+}
